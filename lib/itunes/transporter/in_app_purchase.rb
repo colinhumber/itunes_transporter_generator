164661c0bd68a316034536018c91d0d0b4a4c3aa
Colin Humber <colinhumber@gmail.com>
2012-11-28 15:03:11 -0700

    Cleanup

--- lib/itunes/transporter/in_app_purchase.rb
+++ lib/itunes/transporter/in_app_purchase.rb
@@ -1,22 +1,15 @@
 module Itunes
   module Transporter
-    
-    CONSUMABLE = 'consumable'
-    NON_CONSUMABLE = 'non-consumable'
-    SUBSCRIPTION = 'subscription'
-    FREE_SUBSCRIPTION = 'free-subscription'
-
-    DURATION_7_DAYS = '7 Days'
-    DURATION_1_MONTH = '1 Month'
-    DURATION_2_MONTHS = '2 Months'
-    DURATION_3_MONTHS = '3 Months'
-    DURATION_6_MONTH = '6 Months'
-    DURATION_1_YEAR = '1 Year'
-
     class InAppPurchase < Struct.new(:product_id, :reference_name, :type, :duration, :free_trial_duration, :bonus_duration, :products, :locales, :review_screenshot_image, :review_notes, :should_remove)
+			def to_s
+				"Product ID: #{self.product_id} Reference Name: #{self.reference_name}"
+			end
     end
 
-    class Product < Struct.new(:cleared_for_sale, :intervals, :should_remove, :wholesale_price_tier)
+    class Product < Struct.new(:cleared_for_sale, :intervals, :should_remove, :wholesale_price_tier)=
+			def to_s
+				"Cleared for sale: #{self.cleared_for_sale}, Tier: #{self.wholesale_pricing_tier}"
+			end
     end
 
     class Interval < Struct.new(:start_date, :end_date, :wholesale_price_tier)
