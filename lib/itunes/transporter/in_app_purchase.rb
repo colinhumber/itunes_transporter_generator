d50c52a45ef47a849cb4b798bb17bcd7393b6b30
Luke Hutscal <luke@creaturecreative.com>
2012-11-28 14:52:38 -0700

    Converted tabs to spaces

--- lib/itunes/transporter/in_app_purchase.rb
+++ lib/itunes/transporter/in_app_purchase.rb
@@ -1,28 +1,28 @@
 module Itunes
-	module Transporter
-		
-		CONSUMABLE = 'consumable'
-		NON_CONSUMABLE = 'non-consumable'
-		SUBSCRIPTION = 'subscription'
-		FREE_SUBSCRIPTION = 'free-subscription'
+  module Transporter
+    
+    CONSUMABLE = 'consumable'
+    NON_CONSUMABLE = 'non-consumable'
+    SUBSCRIPTION = 'subscription'
+    FREE_SUBSCRIPTION = 'free-subscription'
 
-		DURATION_7_DAYS = '7 Days'
-		DURATION_1_MONTH = '1 Month'
-		DURATION_2_MONTHS = '2 Months'
-		DURATION_3_MONTHS = '3 Months'
-		DURATION_6_MONTH = '6 Months'
-		DURATION_1_YEAR = '1 Year'
+    DURATION_7_DAYS = '7 Days'
+    DURATION_1_MONTH = '1 Month'
+    DURATION_2_MONTHS = '2 Months'
+    DURATION_3_MONTHS = '3 Months'
+    DURATION_6_MONTH = '6 Months'
+    DURATION_1_YEAR = '1 Year'
 
-		class InAppPurchase < Struct.new(:product_id, :reference_name, :type, :duration, :free_trial_duration, :bonus_duration, :products, :locales, :review_screenshot_image, :review_notes, :should_remove)
-		end
+    class InAppPurchase < Struct.new(:product_id, :reference_name, :type, :duration, :free_trial_duration, :bonus_duration, :products, :locales, :review_screenshot_image, :review_notes, :should_remove)
+    end
 
-		class Product < Struct.new(:cleared_for_sale, :intervals, :should_remove, :wholesale_price_tier)
-		end
+    class Product < Struct.new(:cleared_for_sale, :intervals, :should_remove, :wholesale_price_tier)
+    end
 
-		class Interval < Struct.new(:start_date, :end_date, :wholesale_price_tier)
-		end
+    class Interval < Struct.new(:start_date, :end_date, :wholesale_price_tier)
+    end
 
-		class PurchaseLocale < Struct.new(:name, :title, :description, :publication_name)
-		end
-	end
+    class PurchaseLocale < Struct.new(:name, :title, :description, :publication_name)
+    end
+  end
 end
